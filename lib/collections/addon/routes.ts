fb14a93d457629a7ec88075af2838a87e30f2606
Barrett K. Harber <barrett.harber@gmail.com>
2018-08-22 17:49:29 -0400

    Move some components to app-components

--- lib/collections/addon/routes.ts
+++ lib/collections/addon/routes.ts
@@ -1,8 +1,8 @@
 import buildRoutes from 'ember-engines/routes';
 
 export default buildRoutes(function() {
-    this.route('discover');
-    this.route('submit');
+    // this.route('discover');
+    // this.route('submit');
 
     this.route('provider', { path: '/:slug' }, function() {
         this.route('discover');
